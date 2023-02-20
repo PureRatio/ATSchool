ee504d6b8a772f92fd65f9572cb96b9e2bb430d0
Ariadna Illarionova <ariionova@gmail.com>
2023-02-20 20:55:51 +0700

    Some changes in fifth task

--- src/twelfth/FifthTask.java
+++ src/twelfth/FifthTask.java
@@ -5,31 +5,30 @@ import java.util.Random;
 public class FifthTask {
     public static void main(String[] args) {
         Random rng = new Random();
-        int[][] array = new int[rng.nextInt(6)][];
+        int[][] arr = new int[rng.nextInt(6)][];
 
-        for(int i = 0; i < array.length; i++) {
-            array[i] = new int[rng.nextInt(8)];
-            fillArray(array[i]);
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = new int[rng.nextInt(8)];
+            fillArray(arr[i]);
         }
 
-        sortBySumm(array);
+        System.out.println("Вывод исходного массива:");
+        printJaggedArr(arr);
 
-        for(int i = 0; i < array.length; i++) {
-            for(int j = 0; j < array[i].length; j++) {
-                System.out.print(array[i][j] + " ");
-            }
-            System.out.print("\n");
-        }
+        sortBySumm(arr);
+
+        System.out.println("Вывод отсортированного массива:");
+        printJaggedArr(arr);
     }
 
-    public static void fillArray(int[] array) {
+    static void fillArray(int[] array) {
         Random rng = new Random();
         for(int i = 0; i < array.length; i++){
             array[i] = rng.nextInt(19) - 9;
         }
     }
 
-    public static int summ(int[] array) {
+    static int summ(int[] array) {
         int result = 0;
         for(int num : array) {
             result += num;
@@ -37,7 +36,7 @@ public class FifthTask {
         return result;
     }
 
-    public static void sortBySumm(int[][] arr) {
+    static void sortBySumm(int[][] arr) {
         for (int i=0;i<arr.length-1;++i){
             for(int j=0;j<arr.length-i-1; ++j){
                 if(summ(arr[j+1]) > summ(arr[j])){
@@ -46,6 +45,17 @@ public class FifthTask {
                     arr[j+1] = swap;
                 }
             }
+        }
+    }
+
+    static void printJaggedArr(int[][] arr) {
+        System.out.println("Rows count: " + arr.length);
+        for(int i = 0; i < arr.length; i++) {
+            System.out.printf("Columns count: %d, summ: %d {", arr[i].length, summ(arr[i]));
+            for(int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.print("}\n");
         }
     }
 }
