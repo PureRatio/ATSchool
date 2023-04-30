eb193a61e7420b9ae86fbb23ddf85bd7d3a310de
Ariadna Illarionova <ariionova@gmail.com>
2023-05-01 01:17:50 +0700

    Added changes for Operations task

--- src/sixteenth/Program.java
+++ src/sixteenth/Program.java
@@ -2,22 +2,22 @@ package sixteenth;
 
 public class Program {
     public static void main(String[] args) {
-        if(args.length == 0){
+        if (args.length == 0) {
             System.out.println("Не передано название операции.");
             return;
         }
 
-        if (args.length == 1){
+        if (args.length == 1) {
             System.out.println("Не переданы числа для операции.");
             return;
         }
 
         int[] array = new int[args.length - 1];
 
-        for (int i = 1; i < args.length; i++){
+        for (int i = 1; i < args.length; i++) {
             try {
-                array[i-1] = Integer.parseInt(args[i]);
-            } catch (Exception e){
+                array[i - 1] = Integer.parseInt(args[i]);
+            } catch (Exception e) {
                 System.out.printf("%s не число\n", args[i]);
                 return;
             }
@@ -26,7 +26,7 @@ public class Program {
         Program program = new Program();
         Function function;
         program.printArray(array);
-        switch (args[0]){
+        switch (args[0]) {
             case "Half":
                 function = new Half();
                 break;
@@ -39,24 +39,46 @@ public class Program {
             case "Square":
                 function = new Square();
                 break;
+            case "Wave":
+                function = new Function() {
+                    private int previousNum = 0;
+
+                    @Override
+                    public int evaluate(int num) {
+                        int result = previousNum + num;
+                        previousNum = num;
+                        return result;
+                    }
+                };
+                break;
+            case "SquareEven":
+                function = (int num) -> {
+                    if (num % 2 == 0) {
+                        return num * num;
+                    }
+                    return num;
+                };
+                break;
             default:
                 System.out.printf("Операция %s не поддерживается\n", args[0]);
                 return;
         }
+
         array = program.applyFunction(array, function);
         program.printArray(array);
     }
 
-    void printArray(int[] arr){
-        for(int num : arr){
+    void printArray(int[] arr) {
+        for (int num : arr) {
             System.out.print(num);
             System.out.print(" ");
         }
         System.out.println();
     }
-    int[] applyFunction(int[] arr, Function function){
+
+    int[] applyFunction(int[] arr, Function function) {
         int[] result = new int[arr.length];
-        for (int i = 0; i < arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             result[i] = function.evaluate(arr[i]);
         }
         return result;
