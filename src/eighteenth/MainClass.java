f2260f49c47249c1232ba56b6ef4cf9594d0cce7
Ariadna Illarionova <ariionova@gmail.com>
2023-05-31 00:22:34 +0700

    Added first task

--- src/eighteenth/MainClass.java
+++ src/eighteenth/MainClass.java
@@ -4,20 +4,22 @@ import eighteenth.vegetables.*;
 import eighteenth.fruits.*;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class MainClass {
     public static void main(String[] args) {
         ArrayList <Plant> plants = new ArrayList <> ();
-        HashMap <String, Plant> hashMapPlants = new HashMap <> ();
+        ArrayList <Fruit> multiFruit = new ArrayList <> ();
 
         plants.add(new Banana());
         plants.add(new Apple());
         plants.add(new Cucumber());
         plants.add(new Tomato());
 
-        plants.forEach(plant -> hashMapPlants.put(plant.getName(), plant));
+        plants.stream()
+                .filter(Fruit.class::isInstance)
+                .map(Fruit.class::cast)
+                .forEach(multiFruit::add);
 
-        hashMapPlants.forEach((key, value) -> System.out.println(key + " " + value.getClass().getSimpleName()));
+        multiFruit.forEach(x -> System.out.println(x.getName()));
     }
 }
